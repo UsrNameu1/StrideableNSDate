e78eff7485b1e3facd0e5e1eb3af837c3c76512b
adachi.yuichi <adachi.yuichi@classmethod.jp>
2014-12-08 00:22:22 +0900

    Initial Commit

---
+++ StrideableNSDate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StrideableNSDate
+//
+//  Created by adachi.yuichi on 2014/12/08.
+//  Copyright (c) 2014年 yad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
